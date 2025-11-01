9e267b1ca65b753b7c7b011764f862883ac3d8e1
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2025-11-01 18:27:31 +0100

    Improved integration for later stages

--- integration/3A_integrate_with_class.py
+++ integration/3A_integrate_with_class.py
@@ -1,4 +1,6 @@
-import os, sys
+import os
+import sys
+
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, ROOT)
 
@@ -17,29 +19,41 @@ m = spark.read.option("header", True).csv(main_file.as_posix())
 c = spark.read.option("header", True).csv(class_file.as_posix())
 
 c = (
-    c.select("Code", "Region", "Income group", "Lending category")
+    c.select(F.col("Economy").alias("country_name_join"), "Code", "Region", "Income group", "Lending category")
     .withColumn("code", F.upper(F.trim(F.col("Code"))))
     .dropDuplicates(["code"])
 )
 
-m2 = m.withColumn("economy_norm", F.upper(F.trim(F.col("economy"))))
-joined = m2.join(c, F.col("economy_norm") == F.col("code"), "left")
-joined = joined.withColumn("Country name", F.col("Code"))
+m2 = m.withColumn("economy_join", F.upper(F.trim(F.col("economy"))))
+joined = m2.join(c, F.col("economy_join") == F.col("code"), "left")
+joined = joined.withColumn("Country name", F.col("country_name_join"))
 
-keep_main = [col for col in m.columns]
+keep_main = [col for col in m.columns if col != "is_valid" and not col.startswith("YR")]
 keep_class = ["Region", "Income group", "Lending category"]
+year_cols = [c for c in joined.columns if c.startswith("YR")]
 
-out = joined.select(*[F.col(f"`{c}`") for c in keep_main], *[F.col(c) for c in keep_class])
+ordered_cols = (
+        [F.col(f"`{c}`") for c in keep_main] +
+        [F.col(c) for c in keep_class] +
+        [F.col("is_valid")] +
+        [F.col(c) for c in year_cols]
+)
+
+out = joined.select(*ordered_cols)
 if "Code" in out.columns:
     out = out.drop("Code")
 if "code" in out.columns:
     out = out.drop("code")
-if "economy_norm" in out.columns:
-    out = out.drop("economy_norm")
+if "economy_join" in out.columns:
+    out = out.drop("economy_join")
+if "country_name_join" in out.columns:
+    out = out.drop("country_name_join")
+
+out.show(truncate=False)
 
 (out.coalesce(1)
-   .write.mode("overwrite")
-   .option("header", True)
-   .csv((out_dir / "3A_integrated_with_class.csv").as_posix()))
+ .write.mode("overwrite")
+ .option("header", True)
+ .csv((out_dir / "3A_integrated_with_class.csv").as_posix()))
 
 spark.stop()
