d2fb3f7d3341cfbc5bf8406dbaf9b8ef5e44ecf8
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2025-11-17 21:51:29 +0100

    Added categorical outliers identification script based on distinct values observations and made small modifications on other stages

--- phase_2/1AA_categorial_outliers_identification.py
+++ phase_2/1AA_categorial_outliers_identification.py
@@ -1,5 +1,6 @@
 import os
 
+from pyspark.sql import functions as F
 from pyspark.sql import SparkSession
 from utils.paths import phase1_path, phase2_path
 from utils.schema import schema
@@ -17,13 +18,14 @@ df = spark.read.format("csv") \
     .schema(schema) \
     .load(input_file_path)
 
-year_cols = [c for c in df.columns if c.startswith("YR")]
+df = df.filter(
+    (F.col("SEX") == "students can’t really change how intelligent they are\"\"\"")
+    | (F.col("UNIT_MEASURE") == "SE.PRM.TINM.8")
+    | (F.col("economy") == "(De Facto) Percent of teachers that agree or strongly agrees with \"\"To be honest")
+    | (F.col("URBANIZATION") == "RURURUR")
+    | (F.col("INDICATOR") == "System.IO.MemoryStream")
+)
 
-non_year_cols = [c for c in df.columns if c not in year_cols]
+df.show(truncate=False, n=df.count())
 
-for col in non_year_cols:
-    distinct_df = df.select(col).distinct()
-    output_path = os.path.join(output_dir_path, f"distinct_values_{col}.csv")
-
-    distinct_df.coalesce(1).write.mode("overwrite").option("header", True).csv(output_path)
-    print(f"Saved distinct values for column '{col}' to {output_path}")
+print(df.count())
